1d25d11268a105e9decf751e20c56e4a09e833e5
LEB <LEB@localhost>
2021-09-27 12:29:40 +0200

    Oppstartsfiler

---
+++ dat108-oblig2/src/no/hvl/dat108/oppgave1/Oppg1a.java
@@ -0,0 +1,14 @@
+package no.hvl.dat108.oppgave1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Oppg1a {
+
+	public static void main(String[] args) {
+		
+		List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
+
+	}
+
+}
